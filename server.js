f8a5f39cfcc7f17a2060aa435bc27edc4af55e9e
Luong Tran <32104868+tranvanluong98@users.noreply.github.com>
2018-09-18 21:34:56 +0700

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(session({
 
 }))
 
-app.use(cors({ origin: [ 'http://localhost:3000' ], credentials: true }));
+app.use(cors({ origin: [ 'https://battlenow.herokuapp.com ' ], credentials: true }));
 
 app.use("/api", apiRouter);
 var UserList = [
